5b769cb83c05e266c4b0273777dc277a589dda03
Abel Mescua <abelmescua@gmail.com>
2020-09-09 16:08:12 +0200

    Project initial setup

---
+++ src/test/java/com/cyan/amescua/AmescuaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cyan.amescua;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmescuaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
