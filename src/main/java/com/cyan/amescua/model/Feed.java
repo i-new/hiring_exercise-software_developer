92b2af2d4ff69e5e25e61386b6d1ff7a31fa0bfa
Abel Mescua <abelmescua@gmail.com>
2020-09-10 17:29:32 +0200

    adding XMLService to retrieve and parse XML urls

--- src/main/java/com/cyan/amescua/model/Feed.java
+++ src/main/java/com/cyan/amescua/model/Feed.java
@@ -1,6 +1,7 @@
 package com.cyan.amescua.model;
 
 import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 public class Feed {
@@ -9,8 +10,28 @@ public class Feed {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    private String title;
+    private String link;
+    private String pubDate;
+
     @Column(unique = true)
     private String description;
 
+    public Feed(String title, String link, String pubDate, String description) {
+        this.title = title;
+        this.link = link;
+        this.pubDate = pubDate;
+        this.description = description;
+    }
 
+    @Override
+    public String toString() {
+        return "Feed{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", link='" + link + '\'' +
+                ", pubDate=" + pubDate +
+                ", description='" + description + '\'' +
+                '}';
+    }
 }
---
+++ src/main/java/com/cyan/amescua/services/XMLService.java
@@ -0,0 +1,71 @@
+package com.cyan.amescua.services;
+
+import com.cyan.amescua.model.Feed;
+import org.springframework.stereotype.Service;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * This service will be used to get xml's API URLs and parse them into a Feed object
+ */
+@Service
+public class XMLService {
+
+    /**
+     * This method gets a url for XML feeds and retrieve a list of Feed objects from those XML data.
+     * @return List<Feed>
+     */
+    public List<Feed> parseFeeds() {
+
+        List<Feed> feeds = new ArrayList<>();
+
+        String URL = "https://news.google.com/rss?cf=all&hl=en-US&pz=1&gl=US&ceid=US:en";
+
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        try {
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document doc = builder.parse(URL);
+
+            // normalize XML Response
+            doc.getDocumentElement().normalize();
+
+            // Retrieve all the feeds
+            NodeList nodeList = doc.getElementsByTagName("item");
+
+            for (int i = 0; i < nodeList.getLength(); i++) {
+
+                Node node = nodeList.item(i);
+
+                if (node.getNodeType() == Node.ELEMENT_NODE) {
+                    Element elem = (Element) node;
+                    feeds.add(
+                            new Feed(elem.getElementsByTagName("title").item(0).getTextContent(),
+                            elem.getElementsByTagName("link").item(0).getTextContent(),
+                            elem.getElementsByTagName("pubDate").item(0).getTextContent().toString(),
+                            elem.getElementsByTagName("description").item(0).getTextContent())
+                    );
+                }
+            }
+
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return feeds;
+    }
+}
