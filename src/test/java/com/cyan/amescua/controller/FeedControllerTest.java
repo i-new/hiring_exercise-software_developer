ebf896ebaf9ee44c831dd5f211ed8eb2406e7ed9
Abel Mescua <abelmescua@gmail.com>
2020-09-14 12:26:42 +0200

    adding unit test for FeedController endpoints

---
+++ src/test/java/com/cyan/amescua/controller/FeedControllerTest.java
@@ -0,0 +1,48 @@
+package com.cyan.amescua.controller;
+
+import com.cyan.amescua.services.FeedService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(controllers = FeedController.class)
+class FeedControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private FeedService feedService;
+
+    @Test
+    void retrieveRSS() throws Exception {
+        mockMvc.perform(get("/analyse/new")
+            .contentType("application/json")
+            .param("url1", "https://news.google.com/rss?cf=all&hl=en-US&pz=1&gl=US&ceid=US:en")
+            .param("url2", "https://www.api.org/news-policy-and-issues/news-feed"))
+            .andExpect(status().isOk());
+    }
+
+    @Test
+    void retrieveResults() throws Exception {
+        mockMvc.perform(get("/frequency/1")
+                .contentType("application/json"))
+                .andExpect(status().isNotFound());
+    }
+}
